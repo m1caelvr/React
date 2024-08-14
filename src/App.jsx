3153f3dcaf762034c43e094f42e7a436af4086d0
Micael-Rodrigues <micaelvitor709@gmail.com>
2024-08-14 18:33:16 -0300

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
+import './global.css'
+
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
-import './global.css'
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
     <div>
-      <Header /> <br />
+      <Header />
 
-      <Post
-        author="Micael Vitor"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam ducimus, quas, vel eaque facere voluptatibus consequatur quidem fuga consequuntur a asperiores consectetur excepturi accusantium quae, maxime iure exercitationem nisi sint."
-      />
-      <Post
-        author="Autor 2"
-        content="Esse é um conteúdo"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Micael Vitor"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam ducimus, quas, vel eaque facere voluptatibus consequatur quidem fuga consequuntur a asperiores consectetur excepturi accusantium quae, maxime iure exercitationem nisi sint."
+          />
+          <Post
+            author="Autor 2"
+            content="Esse é um conteúdo"
+          />
+        </main>
+
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import { PencilLine } from '@phosphor-icons/react'
+
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1498049860654-af1a5c566876?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
+        
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?q=50&w=100&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+
+                <strong>Micael Vitor</strong>
+                <span>Web developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
