e80da0c6869f7349f292d67e5968d187a288593e
Micael-Rodrigues <micaelvitor709@gmail.com>
2024-08-14 17:32:30 -0300

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+
+import './global.css'
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <div>
+      <Header /> <br />
+
+      <Post
+        author="Micael Vitor"
+        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam ducimus, quas, vel eaque facere voluptatibus consequatur quidem fuga consequuntur a asperiores consectetur excepturi accusantium quae, maxime iure exercitationem nisi sint."
+      />
+      <Post
+        author="Autor 2"
+        content="Esse é um conteúdo"
+      />
+    </div>
   )
 }
 
