78835cd3ebad360dc92f3881686a18db35f6bd53
Micael-Rodrigues <micaelvitor709@gmail.com>
2024-08-15 00:53:42 -0300

    Initial insert comments

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,51 @@ import styles from "./App.module.css";
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/m1caelvr.png',
+      name: 'Micael Vitor',
+      role: 'Desenvolvedor WEB',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-08-14 20:52:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-05-10 20:52:00'),
+  },
+  {
+    id: 3,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Maykbrito',
+      role: 'Desenvolvedor',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-08-15 00:22:00'),
+  },
+];
+
 function App() {
   return (
     <div>
@@ -15,9 +60,15 @@ function App() {
         <Sidebar />
 
         <main>
-          <Post />
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
 
       </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,48 +1,71 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale/pt-BR';
+
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
-import styles from "./Post.module.css";
 
-export function Post(props) {
-    const handleSubmit = (event) => {
+import styles from "./Post.module.css";
+import { useState } from 'react';
+
+export function Post({ author, publishedAt, content }) {
+    const [comments, setComents] = useState([
+        1, 2,
+    ])
+
+    const publishedDateFormated = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    })
+    
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    })
+
+    function handleCreateNewComment() {
         event.preventDefault();
+
+        setComents([...comments, comments.length + 1])
     };
 
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/maykbrito.png" />
+                    <Avatar src={author.avatarUrl} />
                     
                     <div className={styles.authorInfo}>
-                        <strong>Micael Vitor</strong>
-                        <span>Web developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
                 <time
-                    title="14 de agosto de 2024"
-                    dateTime="2024-08-14" >
-                        Publicado há 1h
+                    title={publishedDateFormated}
+                    dateTime={publishedAt.toISOString()}> 
+                        {publishedDateRelativeToNow}
                 </time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉 <a href="http://jane.design/doctorcare" target="_blank" rel="noopener noreferrer">jane.design/doctorcare</a></p>
-                <p>
-                    <a href="">#novoprojeto </a>
-                    <a href="">#nlw </a>
-                    <a href="">#rocketseat</a>
-                </p>
+                {content.map(line => {
+                    if (line.type === 'paragraph') {
+                        return <p>{line.content}</p>
+                    } else if (line.type === 'link') {
+                        return (
+                            <p>
+                                <a href="#" rel="noopener noreferrer">
+                                    {line.content}
+                                </a>
+                            </p>
+                        )
+                    }
+                })}
             </div>
 
-            <form className={styles.commentForm} onSubmit={handleSubmit}>
+            <form className={styles.commentForm} onSubmit={handleCreateNewComment}>
                 <strong>Deixe um comentário</strong>
 
-                <textarea
-                    placeholder="Deixe um comentário"
-                />
+                <textarea placeholder="Deixe um comentário" />
 
                 <footer>
                     <button type="submit">Publicar</button>
@@ -50,8 +73,9 @@ export function Post(props) {
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
+                {comments.map(coment => {
+                    return <Comment />
+                })}
             </div>
         </article>
     )
